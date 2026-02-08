93988dcbaaf3a93eb6e94fc95c37a10474453392
laura3212401957 <lcvalentina3212401957@gmail.com>
2026-02-08 14:53:29 -0500

    Cambios

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const juegos = [
   { nombre: "Marvel Rivals", imagen: "img/marvel.jpg" },
   { nombre: "Mortal Kombat 11", imagen: "img/mk11.jpg" },
   { nombre: "Peak", imagen: "img/peak.jpg" },
-  { nombre: "Read Dead Redemption 2", imagen: "img/rdr2.jpg"}
-
+  { nombre: "Read Dead Redemption 2", imagen: "img/rdr2.jpg"},
+  {nombre: "God Of War Ragnarok", imagen: "img/god_of_war_r.jpg"},
+  {nombre:"God Of War", imagen: "img/god_of_war.jpg"},
+  {nombre:"Cyberpunk 2077", imagen: "img/cyberpunk_2077.jpg"},
+  {nombre: "Detroit Become Human", imagen: "img/detroit.jpg"},
+  {nombre:"Wuthering Waves", imagen: "img/wuwa.jpg"}
 ];
 
 function elegirJuego() {
